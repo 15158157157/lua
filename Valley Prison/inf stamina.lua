06bd91b4acf83de0f0706150758eafa4e61d3600
1412471811456 <exquisitelawyer@gmail.com>
2026-03-27 22:05:38 +0700

    Delete Valley Prison directory

--- Valley Prison/inf stamina.lua
+++
@@ -1,7 +0,0 @@
-local Stamina = game:GetService("Players").LocalPlayer:WaitForChild("ServerVariables"):WaitForChild("Sprint"):WaitForChild("Stamina")
-
-Stamina.Changed:Connect(function()
-    if Stamina.Value == 0 then
-        Stamina.Value = 100
-    end
-end)
