2233b1ad92fe59b16e0385dd7d46206c0a8d58d1
zllssrr <exquisitelawyer@gmail.com>
2026-04-08 22:38:13 +0700

    Update No Recoil.lua

--- Operation: Siege/No Recoil.lua
+++ Operation: Siege/No Recoil.lua
@@ -1,3 +1,8 @@
+if identifyexecutor and (identifyexecutor() == "Solara" or identifyexecutor() == "Xeno") then
+	game:GetService("Players").LocalPlayer:Kick("garbage")
+	return
+end
+
 local oldRequire
 oldRequire = hookfunction(require,function(module)
 	local result = oldRequire(module)
@@ -13,15 +18,6 @@ oldRequire = hookfunction(require,function(module)
 			result.gunRecoil.gunRecoilTilt = {0,0}
 			result.gunRecoil.gunRecoilLeft = {0,0}
 			result.gunRecoil.gunRecoilRight = {0,0}
-		end
-		if result.MinRecoilPower then
-			result.MinRecoilPower = 0
-		end
-		if result.MaxRecoilPower then
-			result.MaxRecoilPower = 0
-		end
-		if result.RecoilPowerStepAmount then
-			result.RecoilPowerStepAmount = 0
 		end
 	end
 	return result
